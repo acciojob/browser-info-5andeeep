cad832557d8e63e73240d97cf7e584e23fd2cd11
Sandeep <newsandeep03@gmail.com>
2023-05-24 18:44:44 +0530

    ans added

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+const browserInfo = document.getElementById("browser-info");
+
+browserInfo.innerText = "You are using " + navigator.appName + " version " + navigator.appVersion
+
+// console.log(navigator);
+// console.log(navigator.appName);
